4dc9817eb859b92999cdb67b1899c2e30ac04017
mohammadhadi <mhmoghaddasin313@gmail.com>
2021-01-13 20:58:11 +0330

    add post restful api

--- blog/api.py
+++ blog/api.py
@@ -1,48 +1,142 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Post, Comment
 from .serializer import PostSerializer, CommentSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+
+
+class PostViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        queryset = queryset.filter(draft=False)
+        return queryset
+
+
+class PostListGeneric(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+
+class PostDetailGeneric(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+
+class PostListMixin(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PostDetailMixin(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class PostList(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @csrf_exempt
+@api_view(['GET', 'POST'])
 def post_list(request):
     if request.method == 'GET':
         post = Post.objects.all()
         serializer = PostSerializer(post, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
+        serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
 
 
 @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def post_detail(request, pk):
     try:
         post = Post.objects.get(pk=pk)
     except Post.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == "GET":
         serializer = PostSerializer(post)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(post, data=data)
+        # data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         post.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @csrf_exempt
 def comment_list(request):
@@ -74,7 +168,7 @@ def comment_detail(request, pk):
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
-        serializer = CommentSerializer(post, data=data)
+        serializer = CommentSerializer(comment, data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from .views import home, post_single, categories_archive, category_single, like_comment, PostArchive, PostDetails, \
-    CategoryDetails , create_comment
-from .api import post_detail, comment_detail, comment_list, post_list
+    CategoryDetails, create_comment
+from .api import comment_detail,\
+    comment_list, PostList,\
+    PostDetail, PostDetailMixin, PostListMixin,\
+    PostListGeneric, PostDetailGeneric, PostViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+
+
+# post_list = PostViewSet.as_view({'get': 'list', 'post': 'create'})
+# post_detail = PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
 
 urlpatterns = [
     path('', home, name='home'),
@@ -12,8 +25,11 @@ urlpatterns = [
     path('categories/<slug:slug>/', CategoryDetails.as_view(), name='category_single'),
     path('like_comment/', like_comment, name='like_comment'),
     path('comments/', create_comment, name='add_comment'),
-    path('json/posts/', post_list, name='post_list'),
-    path('json/posts/<int:pk>', post_detail, name='post_detail'),
+    # path('json/posts/', post_list, name='post_list'),
+    # path('json/posts/<int:pk>', post_detail, name='post_detail'),
     path('json/comments/', comment_list, name='comment_list'),
     path('json/comments/<int:pk>', comment_detail, name='comment_detail'),
+    path('json/', include(router.urls)),
 ]
+# urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
+
